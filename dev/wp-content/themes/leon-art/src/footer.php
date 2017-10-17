2d53ddd6dc45342b803320676b8b89cd76af54f9
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-10-17 17:10:24 +0200

    Finished footer markup

--- dev/wp-content/themes/leon-art/src/footer.php
+++ dev/wp-content/themes/leon-art/src/footer.php
@@ -1,29 +1,46 @@
-<footer class="c-footer o-box">
-    <nav class="c-footer__nav c-nav c-nav--footer">
-        <a href="#" class="c-nav__item">Accueil</a>
-        <a href="#" class="c-nav__item">Programme</a>
-        <a href="#" class="c-nav__item">Artistes</a>
-        <a href="#" class="c-nav__item">Agenda</a>
-        <a href="#" class="c-nav__item">En pratique</a>
-        <a href="#" class="c-nav__item">Actualités</a>
-        <a href="#" class="c-nav__item">Partenaires</a>
-        <a href="#" class="c-nav__item">Nous contacter</a>
-    </nav>
-    <ul class="c-footer__partners">
-        <li>
-            <a href="#">
-                <img src="#" alt="partenaire 1">
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <img src="#" alt="partenaire 2">
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <img src="#" alt="partenaire 3">
-            </a>
-        </li>
-    </ul>
+<footer class="c-footer">
+    <h2 class="u-hidden-visually">Footer</h2>
+    <div class="o-box">
+        <nav class="c-footer__nav c-nav c-nav--footer">
+            <a href="#" class="c-nav__item">Accueil</a>
+            <a href="#" class="c-nav__item">Programme</a>
+            <a href="#" class="c-nav__item">Artistes</a>
+            <a href="#" class="c-nav__item">Agenda</a>
+            <a href="#" class="c-nav__item">En pratique</a>
+            <a href="#" class="c-nav__item">Actualités</a>
+            <a href="#" class="c-nav__item">Partenaires</a>
+            <a href="#" class="c-nav__item">Nous contacter</a>
+        </nav>
+        <ul class="c-footer__partners">
+            <li>
+                <a href="#">
+                    <img src="#" alt="partenaire 1">
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img src="#" alt="partenaire 2">
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img src="#" alt="partenaire 3">
+                </a>
+            </li>
+        </ul>
+        <div>
+            <h2 class="c-colorblock__title">S'inscrire à la newsletter</h2>
+            <?php get_template_part('part', 'newsletter'); ?>
+        </div>
+    </div>
+    <div class="o-box c-footer__socials">
+        <ul>
+            <li><a href="#"><img src="#" alt="Logo de facebook">Facebook</a></li>
+            <li><a href="#"><img src="#" alt="Logo de twitter">Twitter</a></li>
+            <li><a href="#"><img src="#" alt="Logo d'instagram">Instagram</a></li>
+        </ul>
+    </div>
+    <div class="o-box o-box--small c-footer__copyright">
+        <p>© 2017 <a href="http://mehdy.ouras.be">Mehdy Ouras</a></p>
+    </div>
 </footer>
