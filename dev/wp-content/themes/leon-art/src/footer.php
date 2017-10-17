b99ede652e4b1eeb27db85f9bceb28114d1db63f
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-10-17 14:43:28 +0200

    Added footer markup

---
+++ dev/wp-content/themes/leon-art/src/footer.php
@@ -0,0 +1,29 @@
+<footer class="c-footer">
+    <nav class="c-footer__nav c-nav--footer">
+        <a href="#" class="nav__item">Accueil</a>
+        <a href="#" class="nav__item">Programme</a>
+        <a href="#" class="nav__item">Artistes</a>
+        <a href="#" class="nav__item">Agenda</a>
+        <a href="#" class="nav__item">En pratique</a>
+        <a href="#" class="nav__item">Actualités</a>
+        <a href="#" class="nav__item">Partenaires</a>
+        <a href="#" class="nav__item">Nous contacter</a>
+    </nav>
+    <ul class="c-footer__partners">
+        <li>
+            <a href="#">
+                <img src="#" alt="partenaire 1">
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <img src="#" alt="partenaire 2">
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <img src="#" alt="partenaire 3">
+            </a>
+        </li>
+    </ul>
+</footer>
