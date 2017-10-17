259620c8bb2f74fdbe47ff7e92df2164e8a9a644
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-10-17 14:43:36 +0200

    Added header markup

--- dev/wp-content/themes/leon-art/src/header.php
+++ dev/wp-content/themes/leon-art/src/header.php
@@ -10,3 +10,34 @@
     <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
 </head>
+<body>
+<header class="o-layout">
+    <div class="o-layout__item c-hamburger c-hamburger--elastic" tabindex="0" aria-label="Menu" role="button" aria-controls="navigation">
+        <div class="c-hamburger-box">
+            <div class="c-hamburger-inner"></div>
+        </div>
+    </div>
+    <nav id="nav">
+        <a href="#" class="c-nav__item">Accueil</a>
+        <a href="#" class="c-nav__item">Programme</a>
+        <a href="#" class="c-nav__item">Artistes</a>
+        <a href="#" class="c-nav__item">Agenda</a>
+        <a href="#" class="c-nav__item">En pratique</a>
+        <a href="#" class="c-nav__item">à propos</a>
+    </nav>
+    <div class="o-layout__item c-brand c-brand--home">
+        <div class="c-brand__logos">
+            <img src="<?php sl_asset('img/logo.svg'); ?>" alt="Logo du festival Saint Léon'art">
+            <img src="<?php sl_asset('img/logo-text.svg'); ?>" alt="Saint Léon'art">
+        </div>
+        <div class="c-brand__details">
+            <h1>Saint Léon'art</h1>
+            <p class="c-brand__dates">28 - 30 septembre 2018</p>
+            <p class="c-brand__location">Quartier Saint Léonard à Liège</p>
+        </div>
+        <div class="c-brand__cta-container">
+            <a href="#" class="c-cta c-cta--main">Programme</a>
+            <a href="#" class="c-cta c-cta--second">Artistes</a>
+        </div>
+    </div>
+</header>
