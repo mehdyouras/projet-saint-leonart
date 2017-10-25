f42b722357ea4f476a0073c049325e0f1b994559
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2017-10-25 18:40:16 +0200

    Added styles for section titles

--- dev/wp-content/themes/leon-art/src/template-home.php
+++ dev/wp-content/themes/leon-art/src/template-home.php
@@ -5,10 +5,10 @@
 get_header();
 ?>
 
-<main class="c-home">
+<main class="c-main c-home ">
     <section class="o-wrapper u-margin-bottom-large">
-        <h2>Quelques artistes présents</h2>
-        <div class="c-card c-home__artists o-box">
+        <h2 class="c-section__title">Quelques artistes présents</h2>
+        <div class="c-card c-home__artists o-box o-box--small">
             <ul class="c-artists">
                 <?php get_template_part('part', 'artist-excerpt'); ?>
             </ul>
@@ -17,7 +17,7 @@ get_header();
     </section>
 
     <section class="o-wrapper u-margin-bottom-large">
-        <h2>Prochain événement</h2>
+        <h2 class="c-section__title">Prochain événement</h2>
         <div class="c-card c-home__event o-box o-box--small c-event">
             <?php get_template_part('part', 'event-excerpt'); ?>
             <a href="#" class="c-cta c-cta--second">Consulter l'agenda</a>
@@ -25,7 +25,7 @@ get_header();
     </section>
 
     <section class="o-wrapper u-margin-bottom-large">
-        <h2>Dernières actualités</h2>
+        <h2 class="c-section__title">Dernières actualités</h2>
         <div class="c-card c-home__news o-box o-box--small c-news">
             <?php get_template_part('part', 'news-excerpt'); ?>
         </div>
@@ -35,7 +35,7 @@ get_header();
         <?php get_template_part('part', 'newsletter'); ?>
     </section>
     <section class="o-box c-colorblock">
-        <h2>Nos réseaux sociaux</h2>
+        <h2 class="c-section__title">Nos réseaux sociaux</h2>
         <ul>
             <li><a href="#"><img src="#" alt="Logo de facebook">Facebook</a></li>
             <li><a href="#"><img src="#" alt="Logo de twitter">Twitter</a></li>
@@ -43,7 +43,7 @@ get_header();
         </ul>
     </section>
     <section class="o-box c-instagramfeed">
-        <h2>Nos photos Instagram</h2>
+        <h2 class="c-section__title">Nos photos Instagram</h2>
         <ol>
             <li><img src="#" alt="Photo"></li>
             <li><img src="#" alt="Photo"></li>
