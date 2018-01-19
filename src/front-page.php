f97cffde4809575c4a9f3dc070534fce1a61a71e
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-19 13:35:18 +0100

    Added frontpage

---
+++ src/front-page.php
@@ -0,0 +1,75 @@
+<?php get_header(); ?>
+<section>
+    <h2 class="show-for-sr">Contenu principal</h2>
+    <section>
+        <h3>Quelques artistes présents</h3>
+        <div>
+            <ul>
+                <li>
+                    <img src="#" alt="#">
+                    <a href="#">Jean Dujardin</a>
+                    <span>Peintre</span>
+                </li>
+                <li>
+                    <img src="#" alt="#">
+                    <a href="#">Jean Dujardin</a>
+                    <span>Peintre</span>
+                </li>
+                <li>
+                    <img src="#" alt="#">
+                    <a href="#">Jean Dujardin</a>
+                    <span>Peintre</span>
+                </li>
+                <li>
+                    <img src="#" alt="#">
+                    <a href="#">Jean Dujardin</a>
+                    <span>Peintre</span>
+                </li>
+            </ul>
+            <a href="#" class="cta cta__secondary">Voir tous les artistes</a>
+        </div>
+    </section>
+    <section>
+        <h3>Prochain événement</h3>
+        <div>
+            <article>
+                <time>22/09</time>
+                <h4>Une expo dans le quartier</h4>
+                <a href="#" class="readmore">En savoir plus</a>
+            </article>
+            <a href="#" class="cta cta__secondary">Consulter l'agenda</a>
+        </div>
+    </section>
+    <section>
+        <h3>Dernières actualités</h3>
+        <div>
+            <article>
+                <time>22/09</time>
+                <h4>Nouvelles inscriptions</h4>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore, similique assumenda incidunt alias recusandae eos beatae, nulla dolor dolores vitae nam neque doloremque provident consectetur pariatur sint porro quas. In?
+                </p>
+                <a href="#" class="readmore">Lire plus</a>
+            </article>
+            <a href="#" class="cta cta__secondary">Voir toutes les news</a>
+        </div>
+    </section>
+    <section>
+        <h3>S'inscrire à la newsletter</h3>
+        <div>
+            
+        </div>
+    </section>
+    <section>
+        <h3>Nos réseaux sociaux</h3>
+        <div>
+            <ul>
+            </ul>
+        </div>
+    </section>
+    <section>
+        <h3>Nos photos Instagram</h3>
+        <p>Widget instagram</p>
+    </section>
+</section>
+<?php get_footer(); ?>
