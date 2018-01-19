00ea5f43a1b5755f58b4c526db0dd7adb1e059ff
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-19 17:21:31 +0100

    Added first markup

---
+++ src/template-artists.php
@@ -0,0 +1,17 @@
+<?php
+/*
+        Template Name: Page des artistes
+*/
+get_header();
+?>
+
+<section>
+    <header>
+        <h2>Artistes</h2>
+    </header>
+    <div>
+        
+    </div>
+</section>
+
+<?php get_footer(); ?>
