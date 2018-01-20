855efcc0255f24b6354c1be28785aec5aed57027
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-20 16:49:14 +0100

    Added single post markup

---
+++ src/single-post.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<section>
+    <header>
+        <h2>Actualité</h2>
+    </header>
+    <article>
+        <header>
+            <time>22/09</time>
+            <h3>Nouvelles inscriptions !</h3>
+        </header>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum illo nulla voluptatibus aut explicabo fugiat reiciendis repellendus eligendi natus at perferendis libero ipsa ipsum delectus in, itaque, corrupti distinctio praesentium!</p>
+    </article>
+    <a href="#" class="cta cta__primary">Voir toute l'actu</a>
+</section>
+
+<?php get_footer(); ?>
