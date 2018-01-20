caad771897ea909056a483f464f45d90e14618ad
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-20 15:51:16 +0100

    Added contact markup

---
+++ src/template-contact.php
@@ -0,0 +1,37 @@
+<?php 
+/*
+        Template Name: Page de contact
+*/
+    get_header();
+?>
+
+<section>
+    <header>
+        <h2>Contact</h2>
+    </header>
+    <div>
+        <section>
+            <h3>Pourquoi nous contacter ?</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui voluptas sunt laudantium laboriosam tempora accusamus a consectetur nesciunt blanditiis quis eos dolore maiores, tenetur harum quisquam fugiat. Dolorum, expedita accusantium.</p>
+        </section>
+        <article>
+            <h3>Coordonnées de l'ASBL Lencreuse</h3>
+            <address>
+                <span>Caroline Caucheteur</span>
+                <span>Rue Adelin et Jules Husson, 33</span>
+                <span>4020 Liège</span>
+                <span>0494 21 98 31</span>
+                <span>caroline@lencreuse.be </span>
+            </address>
+        </article>
+        <section>
+            <h3>Nous contacter</h3>
+            <form action="#">
+                //
+            </form>
+        </section> 
+        <a href="#" class="cta cta__secondary">Programme 2018</a>
+    </div>
+</section>
+
+<?php get_footer(); ?>
