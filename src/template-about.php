624db0594582a8d1ba4bdb3a4ed32f60687b275b
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-20 15:32:18 +0100

    Added about markup

---
+++ src/template-about.php
@@ -0,0 +1,65 @@
+<?php
+/*
+        Template Name: Page à propos
+*/
+get_header();
+?>
+
+<section>
+    <header>
+        <h2>À propos</h2>
+    </header>
+    <div>
+        <p>
+        Saint Leon'art vise à favoriser la rencontre entre la population du quartier, largement multiculturelle, et l'art sous toutes ses formes, tout en créant des liens durables entre habitants et artistes qui s'y côtoient chaque jours. Cette initiative souhaite également rendre l'art, l'artiste et la culture accessibles à tous, toutes origines, toutes classes sociales confondues, en offrant un accès libre et gratuit à l'événement.
+        </p>
+        <section>
+            <h3>Les organisateurs</h3>
+            <ul>
+                <li>
+                    <img src="#" alt="#">
+                    <a href="#">Jean Dujardin</a>
+                </li>
+                <li>
+                    <img src="#" alt="#">
+                    <a href="#">Jean Dujardin</a>
+                </li>
+                <li>
+                    <img src="#" alt="#">
+                    <a href="#">Jean Dujardin</a>
+                </li>
+                <li>
+                    <img src="#" alt="#">
+                    <a href="#">Jean Dujardin</a>
+                </li>
+            </ul>
+        </section>
+        <section>
+            <h3 class="sr-only">Quelques chiffres</h3>
+            <ul>
+                <li>4 éditions</li>
+                <li>+70 artistes</li>
+                <li>+1500 visiteurs</li>
+                <li>+30 expositions</li>
+            </ul>
+        </section>
+        <a href="#" class="cta cta__secondary">Programme 2018</a>
+        <section>
+            <h3>Les éditions précédentes</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, quisquam, minus obcaecati et sapiente quas libero rem, ullam modi aliquam animi eius recusandae eligendi voluptates nobis? Dolorem architecto voluptatum cum.</p>            
+        </section>
+        <section>
+            <h3>Espace presse</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, commodi. Consequuntur repudiandae est recusandae debitis aperiam ullam laudantium, incidunt aliquam hic optio, iure nesciunt quam harum ad quas magnam laborum.</p>
+            <ul>
+                <li><a href="#">Logos</a> 3 Mo</li>
+                <li><a href="#">Documents</a> 3 Mo</li>
+            </ul>
+        </section>
+        <a href="#" class="cta cta__secondary">Nous contacter</a>
+    </div>
+</section>
+
+<?php 
+    get_footer();
+?>
