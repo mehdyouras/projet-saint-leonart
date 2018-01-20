1b94a0a42bafdfcb25c2b47f0886345d7daa0137
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-20 16:16:07 +0100

    Added single exhibition markup

---
+++ src/single-exhibition.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<section>
+    <header>
+        <h2>Exposition</h2>
+    </header>
+    <article>
+        <header>
+            <time>28/09 12h à 28/09 19h</time>
+            <h3>Exposition de Jean Dujardin</h3>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium reiciendis repellendus temporibus repellat error odio consectetur nam quis, optio iusto. Eligendi, facilis deserunt incidunt corporis aliquam neque cum ut veritatis.
+            </p>
+        </header>
+        <img src="#" alt="#">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae odio quisquam consequatur corporis quam similique quas obcaecati temporibus minima. Vero amet libero natus error facilis culpa at maiores asperiores nobis.</p>
+        <a href="#" class="cta cta__primary">Retour au programme</a>
+        <a href="#" class="cta cta__primary">Vers l'artiste</a>
+    </article>
+</section>
+
+<?php get_footer(); ?>
