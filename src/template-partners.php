bd3f164eb73efbcea97d5f4d08e460e6bf0cd9a4
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-20 15:38:09 +0100

    Added parteners markup

---
+++ src/template-partners.php
@@ -0,0 +1,37 @@
+<?php 
+/*
+        Template Name: Page des partenaires
+*/
+    get_header();
+?>
+
+<section>
+    <h2>Partenaires</h2>
+    <div>
+        <ul>
+            <li>
+                <article>
+                    <h3>Partenaire 1</h3>
+                    <img src="#" alt="#">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium pariatur recusandae minima maiores fugit blanditiis error consequuntur! Error neque repellendus harum quam laboriosam obcaecati praesentium! Eveniet deleniti voluptates natus impedit?</p>
+                </article>
+            </li>
+            <li>
+                <article>
+                    <h3>Partenaire 2</h3>
+                    <img src="#" alt="#">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi doloribus sint nulla voluptate ipsa. Culpa ipsam perferendis quos dolore quisquam iure eius excepturi eum esse dolor deleniti, odit totam voluptatibus.</p>
+                </article>
+            </li>
+            <li>
+                <article>
+                    <h3>Partenaire 3</h3>
+                    <img src="#" alt="#">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga laudantium at optio iusto aut necessitatibus eum nobis, officiis consequatur quia architecto, nesciunt dicta. Totam corrupti voluptatum neque eligendi maiores beatae.</p>
+                </article>
+            </li>
+        </ul>
+    </div>
+</section>
+
+<?php get_footer(); ?>
