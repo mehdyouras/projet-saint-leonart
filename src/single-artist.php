8c0b4cf187630779f99b71d7b250747e89d60a31
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-20 12:45:51 +0100

    Added single-artist markup

---
+++ src/single-artist.php
@@ -0,0 +1,60 @@
+<?php
+    get_header();
+?>
+
+<article>
+    <header>
+        <h2>Jean Dujardin</h2>
+    </header>
+    <div>
+        <div>
+            <img src="#" alt="fz">
+            <strong>Jean Dujardin</strong>
+            <p>
+                Artiste peintre
+            </p>
+            <ul>
+                <a href="#"><span class="sr-only">Facebook</span></a>
+                <a href="#"><span class="sr-only">Twitter</span></a>
+                <a href="#"><span class="sr-only">Instagram</span></a>
+            </ul>
+            <address>
+                <span>Rue neuve 5, Huy</span>
+                <span>jean@dujardin.be</span>
+                <span>0476 78 91 56</span>
+                <span>jean.dujardin.be</span>
+            </address>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium recusandae numquam sed, optio at perferendis, ullam dignissimos libero quae fugit saepe doloremque consequuntur sapiente repellendus consectetur. Repudiandae ipsa exercitationem harum!</p>
+            <section>
+                <h3>Jean Dujardin sur le festival</h3>
+                <ol>
+                    <li>
+                        <article>
+                            <img src="http://fakeimg.pl/250x100/" alt="">
+                            <div>
+                                <time>Vendredi 28 à 12h</time>
+                                <time>Samedi 29 à 19h</time>
+                            </div>
+                            <div>
+                                <h3>
+                                    Exposition de Jean Dujardin
+                                </h3>
+                            </div>
+                        </article>
+                    </li>
+                </ol>
+            </section>
+            <section>
+                <h3>Quelques photos de ses réalisations</h3>
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+                <img src="" alt="">
+            </section>
+            <a href="#" class="cta cta__secondary">Voir les autres artistes</a>
+            <a href="#" class="cta cta__secondary">Consulter le programme</a>
+        </div>
+    </div>
+</article>
+
+<?php get_footer(); ?>
