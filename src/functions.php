0222766c31c68723867fae542b8eb7d3e9e31b90
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-21 18:31:55 +0100

    Added infos option page

--- src/functions.php
+++ src/functions.php
@@ -85,7 +85,18 @@ if( function_exists('acf_add_options_page') ) {
 	/* (string) The title displayed in the wp-admin sidebar. Defaults to page_title */
     'menu_title' => 'Partenaires',
     
-    'position' => 5
+    'position' => 6
+	
+    ]);
+
+    acf_add_options_page([
+            /* (string) The title displayed on the options page. Required. */
+        'page_title' => 'Informations générales',
+        
+        /* (string) The title displayed in the wp-admin sidebar. Defaults to page_title */
+        'menu_title' => 'Informations',
+        
+        'position' => 5
 	
     ]);
 	
