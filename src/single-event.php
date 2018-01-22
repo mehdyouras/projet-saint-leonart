9224e5a123d0b400228fb139c2b7e6983ab852d3
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-22 21:39:52 +0100

    Added event php

--- src/single-event.php
+++ src/single-event.php
@@ -1,21 +1,30 @@
-<?php get_header(); ?>
+<?php
+get_header();
+$artists = get_field('event_artists')
+?>
 
 <section>
     <header>
-        <h2>Événement</h2>
+        <h2><?php the_field('event_name') ?></h2>
     </header>
     <article>
         <header>
-            <time>28/09 12h à 28/09 19h</time>
-            <h3>Exposition de Jean Dujardin</h3>
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laudantium reiciendis repellendus temporibus repellat error odio consectetur nam quis, optio iusto. Eligendi, facilis deserunt incidunt corporis aliquam neque cum ut veritatis.
-            </p>
+            <time><?php the_field('event_start'); ?> à <?php the_field('event_end'); ?></time>
+            <h3><?php the_field('envet_name'); ?></h3>
+            <div>
+                <?php foreach($artists as $artist): ?>
+                <a href="<?php the_permalink($artist->ID) ?>"><?php the_field('artist_name', $artist->ID) ?></a>
+                <?php endforeach; ?>
+            </div>
+            <?php $terms = get_field('event_type'); ?>
+            <?php foreach($terms as $key => $term) : ?>
+            <?= $term->name; if(count($terms) > 1 && ($key + 1) !== count($terms)):echo ','; endif;  ?>
+            <?php endforeach; ?>
         </header>
-        <img src="#" alt="#">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae odio quisquam consequatur corporis quam similique quas obcaecati temporibus minima. Vero amet libero natus error facilis culpa at maiores asperiores nobis.</p>
-        <a href="#" class="cta cta__primary">Retour au programme</a>
-        <a href="#" class="cta cta__primary">Vers l'artiste</a>
+        <div>
+            <?php the_field('event_description'); ?>
+        </div>
+        <a href="<?php sla_the_permalink_by_title('Programme'); ?>" class="cta cta__primary">Retour au programme</a>
     </article>
 </section>
 
--- src/template-program.php
+++ src/template-program.php
@@ -42,7 +42,7 @@ $loop = new WP_Query( $args );
         <a href="<?php sla_the_permalink_by_title('Artistes'); ?>" class="cta cta__primary">Voir tous les artistes</a>
         <a href="<?php sla_the_permalink_by_title('En pratique'); ?>" class="cta cta__secondary">En pratique</a>
         <ol>
-            <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+            <?php while ( $loop->have_posts() ) : $loop->the_post(); $artists = get_field('event_artists'); ?>
             <li>
                 <article>
                     <!-- <img src="http://fakeimg.pl/250x100/" alt=""> -->
@@ -52,8 +52,13 @@ $loop = new WP_Query( $args );
                     </div>
                     <div>
                         <h3>
-                            <?php the_field('event_name'); ?>
+                            <a href="<?php the_permalink() ?>"><?php the_field('event_name'); ?></a>
                         </h3>
+                        <div>
+                            <?php foreach($artists as $artist): ?>
+                            <a href="<?php the_permalink($artist->ID) ?>"><?php the_field('artist_name', $artist->ID) ?></a>
+                            <?php endforeach; ?>
+                        </div>
                         <?php $terms = get_field('event_type'); ?>
                         <?php foreach($terms as $key => $term) : ?>
                         <?= $term->name; if(count($terms) > 1 && ($key + 1) !== count($terms)):echo ','; endif;  ?>
