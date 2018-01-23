aea29d9e1a942fc1f7c0c5b856394d05d5611188
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-23 16:51:15 +0100

    Added css

--- src/template-practical.php
+++ src/template-practical.php
@@ -5,13 +5,15 @@
     get_header();
 ?>
 
-<section>
-    <header>
-        <h2>En pratique</h2>
+<section class="container">
+    <header class="page-header">
+        <h2 class="bg-secondary text-center p-3 text-primary">En pratique</h2>
     </header>
     <div>
         <?php the_field('practical_content'); ?>
-        <a href="<?php sla_the_permalink_by_title('Programme') ?>" class="cta cta__secondary">Consulter le programme</a>
+        <div class="text-center m-4">
+            <a href="<?php sla_the_permalink_by_title('Programme') ?>" class="btn btn-secondary">Consulter le programme</a>
+        </div>
     </div>
 </section>
 
