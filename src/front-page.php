146ae68d0b1d7f803f91fc48f990f1d5d0dbbaad
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-23 16:36:53 +0100

    Partitioned artist

--- src/front-page.php
+++ src/front-page.php
@@ -16,22 +16,7 @@
             <div class="card-body">
                 <ul class="list-unstyled d-flex flex-wrap">
                     <?php while ( $artists->have_posts() ) : $artists->the_post(); ?>
-                    <li class="d-flex flex-column col-6 text-center mb-3">
-                        <a class="portrait portrait--small" href="<?php the_permalink(); ?>">
-                            <?= wp_get_attachment_image( get_field('artist_portrait'), 'sla_portrait_small'); ?>
-                        </a>
-                        <div>
-                            <a class="d-block" href="<?php the_permalink(); ?>">
-                                <?php the_field('artist_name'); ?>
-                            </a>
-                            <?php $terms = get_field('artist_kind'); ?>
-                            <small>
-                                <?php foreach($terms as $key => $term) : ?>
-                                <?= $term->name; if(count($terms) > 1 && ($key + 1) !== count($terms)):echo ','; endif;  ?>
-                                <?php endforeach; ?>
-                            </small>
-                        </div>
-                    </li>
+                    <?php get_template_part( 'part', 'artist' ); ?>
                     <?php endwhile; ?>
                 </ul>
                 <div class="text-center">
