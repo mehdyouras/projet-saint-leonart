e7681a2ab4f3cd083bae3624597e61eedc50432d
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-23 16:36:59 +0100

    Added css to artists

--- src/template-artists.php
+++ src/template-artists.php
@@ -12,24 +12,15 @@ $args = array(
 $loop = new WP_Query( $args );
 ?>
 
-<section>
-    <header>
-        <h2>Artistes</h2>
+<section class="container">
+    <header class="page-header">
+        <h2 class="bg-secondary text-center p-3 text-primary">Artistes</h2>
     </header>
-    <div>
-        <div>
-            <ul>
+    <div class="card mt-4 mb-4">
+        <div class="card-body">
+            <ul class="list-unstyled d-flex flex-wrap">
                 <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
-                <li>
-                    <a href="<?php the_permalink(); ?>">
-                        <?= wp_get_attachment_image( get_field('artist_portrait'), 'sla_portrait_small'); ?>
-                        <?php the_field('artist_name'); ?>
-                    </a>
-                    <?php $terms = get_field('artist_kind'); ?>
-                    <?php foreach($terms as $key => $term) : ?>
-                    <?= $term->name; if(count($terms) > 1 && ($key + 1) !== count($terms)):echo ','; endif;  ?>
-                    <?php endforeach; ?>
-                </li>
+                <?php get_template_part( 'part', 'artist' ); ?>
                 <?php endwhile; ?>
             </ul>
         </div>
