b78c315fe9145950a4044fdcb37838fca36d27f1
Mehdy Ouras <Mehdy.ouraselouad@student.hepl.be>
2018-01-23 18:15:28 +0100

    Added css

--- src/template-about.php
+++ src/template-about.php
@@ -6,53 +6,77 @@ get_header();
 ?>
 
 <section>
-    <header>
-        <h2>À propos</h2>
+    <header class="page-header container">
+        <h2 class="bg-secondary text-center p-3 text-primary">À propos</h2>
     </header>
-    <div>
+    <div class="container">
         <p>
             <?php the_field('about_intro'); ?>
         </p>
         <?php if( have_rows('about_creators') ): ?>
-        <section>
-            <h3>Les organisateurs</h3>
-            <ul>
-                <?php while ( have_rows('about_creators') ) : the_row(); ?>
-                <li>
-                    <?= wp_get_attachment_image( get_sub_field('creator_portrait'),  'sla_portrait_small'); ?>
-                    <span><?php the_sub_field('creator_name'); ?></span>
-                </li>
-                <?php endwhile; ?>
-            </ul>
+        <section class="card mt-4 mb-4">
+            <div class="card-header bg-secondary text-primary">
+                <h3>Les organisateurs</h3>
+            </div>
+            <div class="card-body">
+                <ul class="list-unstyled d-flex flex-wrap">
+                    <?php while ( have_rows('about_creators') ) : the_row(); ?>
+                    <li class="d-flex flex-column col-6 text-center mb-3">
+                        <a class="portrait portrait--small" href="<?php the_permalink(); ?>">
+                            <?= wp_get_attachment_image( get_sub_field('creator_portrait'),  'sla_portrait_small'); ?>
+                        </a>
+                        <div>
+                            <span><?php the_sub_field('creator_name'); ?></span>
+                        </div>
+                    </li>
+                    <?php endwhile; ?>
+                </ul>
+            </div>
         </section>
         <?php endif; ?>
-        <?php if( have_rows('about_stats') ): ?>
-        <section>
+    </div>
+    <?php if( have_rows('about_stats') ): ?>
+    <section class="bg-secondary pt-4">
+        <div class="container">
             <h3 class="sr-only">Quelques chiffres</h3>
-            <ul>
+            <ul class="list-unstyled d-flex flex-wrap mb-0">
                 <?php while ( have_rows('about_stats') ) : the_row(); ?>
-                <li>
-                    <span class="d-block"><?php the_sub_field('stat_value'); ?></span>
-                    <span class="d-block"><?php the_sub_field('stat_name'); ?></span>
+                <li class="col-6 text-center mb-4">
+                    <strong class="d-block"><?php the_sub_field('stat_value'); ?></strong>
+                    <strong class="d-block"><?php the_sub_field('stat_name'); ?></strong>
                 </li>
                 <?php endwhile; ?>
             </ul>
-        </section>
-        <?php endif; ?>
-        <a href="<?php sla_the_permalink_by_title("Programme") ?>" class="cta cta__secondary">Programme 2018</a>
+        </div>
+    </section>
+    <?php endif; ?>
+    <div class="mt-4 text-center">
+        <a href="<?php sla_the_permalink_by_title("Programme") ?>" class="btn btn-secondary">Programme 2018</a>
+    </div>
+    <div class="container mt-4">
         <section>
             <?php the_field('about_other'); ?>
         </section>
         <section>
             <h3>Espace presse</h3>
             <p><?php the_field('press_intro'); ?></p>
-            <ul>
+            <ul class="list-unstyled">
                 <?php while ( have_rows('press_files') ) : the_row(); $file = get_sub_field('file_file');?>
-                <li><a href="<?= $file['url']; ?>"><?php the_sub_field('file_name') ?></a> <?= sla_filesize_formatted($file['id']) ?></li>
+                <li class="d-flex align-items-center justify-content-between mb-2">
+                    <div>
+                        <i class="fas fa-download text-primary mr-2" aria-hidden="true"></i>
+                        <a href="<?= $file['url']; ?>"><?php the_sub_field('file_name') ?></a>
+                    </div>
+                    <small>
+                        <?= sla_filesize_formatted($file['id']) ?>
+                    </small>
+                </li>
                 <?php endwhile; ?>
             </ul>
         </section>
-        <a href="<?php sla_the_permalink_by_title("Contact") ?>" class="cta cta__secondary">Nous contacter</a>
+        <div class="text-center mb-4 mt-4">
+            <a href="<?php sla_the_permalink_by_title("Contact") ?>" class="btn btn-secondary">Nous contacter</a>
+        </div>
     </div>
 </section>
 
